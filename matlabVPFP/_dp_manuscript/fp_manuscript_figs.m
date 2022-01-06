f557a1d1e8d359a3c9746cbdddef40823716dc0a
palme876 <palme876@umn.edu>
2022-01-06 13:22:30 -0600

    save kernels into table

--- matlabVPFP/_dp_manuscript/fp_manuscript_figs.m
+++ matlabVPFP/_dp_manuscript/fp_manuscript_figs.m
@@ -14,91 +14,155 @@ fp_manuscript_periEventTraces();
 %% load encoding model output 
 load("C:\Users\Dakota\Documents\GitHub\FP-analysis\matlabVPFP\broken up code\encoding model\encoding_results\_control\stage7\465\allSubjResultskernel_Shifted_all.mat")
 
-%% Encoding Model Kernels Figure
-
-%Plot of encoding model kernels alongside peri-DS traces:
-%1) Between-subjects and 2) For each individual subject
-
-% Load output from the encoding model for each subject
-
-%Define which Stage/Session to plot
-%Want stage 7 (1s pump onset delay) after meeting criteria
-
-%TODO: Review what was actually run through the model, this is just
-%grabbing output
-
-stage= 7
-
-%want 465nm
-signal= 465
-
-%Parameters we ran through encoding model
-%how much time should you shift back (in seconds)
-time_back=5;
-time_forward=10;
-
-%events corresponding to kernels
-cons={'DS','poxDS','loxDS'}; 
-k= numel(cons);
+%% Save kernels into table
+% easy for plotting later
+kernelTable= table();
+kernelTable.file= cell(length(mat)*(periCueFrames+1),1);
+kernelTable.timeLock= nan(length(mat)*(periCueFrames+1),1); 
+kernelTable.kernelDS= nan(length(mat)*(periCueFrames+1),1); 
+kernelTable.kernelPoxDS= nan(length(mat)*(periCueFrames+1),1); 
+kernelTable.kernelLoxDS= nan(length(mat)*(periCueFrames+1),1); 
 
 
-if stage==7 && signal==465
-    %path to .mat Mean Actual LASSO output for each subj (correlations coefficients)
-    pathLasso= 'C:\Users\Dakota\Documents\GitHub\FP-analysis\matlabVPFP\broken up code\encoding model\encoding_results\stage7\465'
+%path to .mat Mean Actual LASSO output for each subj (correlations coefficients)
+pathLasso= 'C:\Users\Dakota\Documents\GitHub\FP-analysis\matlabVPFP\broken up code\encoding model\encoding_results\stage7\465'
+
+%loop through files and extract data
+cd(pathLasso);
+mat = dir('*.mat');
+% fileInd= 1:periCueFrames; %index for table corresponding to this file
+for file= 1:length(mat)
+    kernel=[]; %clear btwn subjects
     
-    %just figures:
-%     %Figure path to between-subjects mean of all kernels
-%     pathMeanLasso= 'C:\Users\Dakota\Documents\GitHub\FP-analysis\matlabVPFP\broken up code\encoding model\_output\output_stage7_465\Avg kernels across animals'
-% 
-%     %Figure path to latency sorted heatplots
-%     pathHeat= 'C:\Users\Dakota\Documents\GitHub\FP-analysis\matlabVPFP\broken up code\encoding model\_output\output_stage7_465\Latencysorted_heatplots'
-    
-    %loop through files and extract data
-    cd(pathLasso);
-    mat = dir('*.mat');
-    for file= 1:length(mat)
-        %load LASSO results
-        lassoOutput(file) = load(mat(file).name);
-        
-        b= lassoOutput(file).b;
-        
-        %isolate kernels for each eventType
-        %make figure for indidivual subj (1 per file)
-        figure();
-        sgtitle(mat(file).name);
-        for eventType = 1:k
-            kernel=[];
-             %for indexing rows of b easily as we loop through event types and build kernel, keep track  of timestamps (ts) that correspond to this event type 
-              if eventType==1
-                tsThisEvent= 2:(numel(b)/k)+1; %skip first index (intercept)
-              else
-                tsThisEvent= tsThisEvent(end)+1:tsThisEvent(end)+(numel(b)/k); 
-              end
+    %load LASSO results
+    lassoOutput(file) = load(mat(file).name);
 
-           sumTerm= []; %clear between event types
+    b= lassoOutput(file).b;
 
-           for ts= 1:(numel(b)/k) %loop through ts; using 'ts' for each timestamp instead of 'i'
-        %                %this seems to fit- there should be 81 time bins in the example data x 7 event types ~ 567      
-                kernel(ts,eventType) = b(tsThisEvent(ts));
-           end
-           
-           %subplot each kernel
-           timeLock= linspace(-time_back, time_forward, size(kernel,1));
-           subplot(k,1,eventType);
-           plot(timeLock, kernel(:,eventType));
-           title(cons(eventType));
-        end
-        
-        saveFig(gcf, figPath, strcat(mat(file).name,'_encoding_kernels'),figFormats);
+    %isolate kernels for each eventType
+    for eventType = 1:k
+         %for indexing rows of b easily as we loop through event types and build kernel, keep track  of timestamps (ts) that correspond to this event type 
+          if eventType==1
+            tsThisEvent= 2:(numel(b)/k)+1; %skip first index (intercept)
+          else
+            tsThisEvent= tsThisEvent(end)+1:tsThisEvent(end)+(numel(b)/k); 
+          end
 
-  
+       sumTerm= []; %clear between event types
+
+       for ts= 1:(numel(b)/k) %loop through ts; using 'ts' for each timestamp instead of 'i'
+    %                %this seems to fit- there should be 81 time bins in the example data x 7 event types ~ 567      
+            kernel(ts,eventType) = b(tsThisEvent(ts));
+       end
+
+       timeLock= linspace(-time_back, time_forward, size(kernel,1));
+
+    end 
+
+    %iterate table index between files and save data in table
+    if file==1
+        fileInd= 1:periCueFrames+1;
+    else
+        fileInd= fileInd+ periCueFrames+1;
     end
-   
     
-end
+    %assume order of eventType columns is DS, DSpox, DSlox
+    kernelTable.file(fileInd)={mat(file).name};
+    kernelTable.timeLock(fileInd)=timeLock;
+    kernelTable.kernelDS(fileInd)=kernel(:,1);
+    kernelTable.kernelPoxDS(fileInd)=kernel(:,2);
+    kernelTable.kernelLoxDS(fileInd)=kernel(:,3);
+
+end %end file loop (subj)
+
+
+%% 
+% %% Encoding Model Kernels Figure
+% 
+% %Plot of encoding model kernels alongside peri-DS traces:
+% %1) Between-subjects and 2) For each individual subject
+% 
+% % Load output from the encoding model for each subject
+% 
+% %Define which Stage/Session to plot
+% %Want stage 7 (1s pump onset delay) after meeting criteria
+% 
+% %TODO: Review what was actually run through the model, this is just
+% %grabbing output
+% 
+% stage= 7
+% 
+% %want 465nm
+% signal= 465
+% 
+% %Parameters we ran through encoding model
+% %how much time should you shift back (in seconds)
+% time_back=5;
+% time_forward=10;
+% 
+% %events corresponding to kernels
+% cons={'DS','poxDS','loxDS'}; 
+% k= numel(cons);
+% 
+% 
+% if stage==7 && signal==465
+%     %path to .mat Mean Actual LASSO output for each subj (correlations coefficients)
+%     pathLasso= 'C:\Users\Dakota\Documents\GitHub\FP-analysis\matlabVPFP\broken up code\encoding model\encoding_results\stage7\465'
+%     
+%     %just figures:
+% %     %Figure path to between-subjects mean of all kernels
+% %     pathMeanLasso= 'C:\Users\Dakota\Documents\GitHub\FP-analysis\matlabVPFP\broken up code\encoding model\_output\output_stage7_465\Avg kernels across animals'
+% % 
+% %     %Figure path to latency sorted heatplots
+% %     pathHeat= 'C:\Users\Dakota\Documents\GitHub\FP-analysis\matlabVPFP\broken up code\encoding model\_output\output_stage7_465\Latencysorted_heatplots'
+%     
+%     %loop through files and extract data
+%     cd(pathLasso);
+%     mat = dir('*.mat');
+%     for file= 1:length(mat)
+%         %load LASSO results
+%         lassoOutput(file) = load(mat(file).name);
+%         
+%         b= lassoOutput(file).b;
+%         
+%         %isolate kernels for each eventType
+%         %make figure for indidivual subj (1 per file)
+%         figure();
+%         sgtitle(mat(file).name);
+%         for eventType = 1:k
+%             kernel=[];
+%              %for indexing rows of b easily as we loop through event types and build kernel, keep track  of timestamps (ts) that correspond to this event type 
+%               if eventType==1
+%                 tsThisEvent= 2:(numel(b)/k)+1; %skip first index (intercept)
+%               else
+%                 tsThisEvent= tsThisEvent(end)+1:tsThisEvent(end)+(numel(b)/k); 
+%               end
+% 
+%            sumTerm= []; %clear between event types
+% 
+%            for ts= 1:(numel(b)/k) %loop through ts; using 'ts' for each timestamp instead of 'i'
+%         %                %this seems to fit- there should be 81 time bins in the example data x 7 event types ~ 567      
+%                 kernel(ts,eventType) = b(tsThisEvent(ts));
+%            end
+%            
+%            %subplot each kernel
+%            timeLock= linspace(-time_back, time_forward, size(kernel,1));
+%            subplot(k,1,eventType);
+%            plot(timeLock, kernel(:,eventType));
+%            title(cons(eventType));
+%         end
+%         
+%         saveFig(gcf, figPath, strcat(mat(file).name,'_encoding_kernels'),figFormats);
+% 
+%   
+%     end
+%    
+%     
+% end
 
 
 %% Kernels + peri-event plot
+%individual subjects
 stage= 7
 
 %want 465nm
@@ -129,8 +193,8 @@ if stage==7 && signal==465
         
         %isolate kernels for each eventType
         %make figure for indidivual subj (1 per file)
-        figure();
-        sgtitle(mat(file).name);
+%         figure();
+%         sgtitle(mat(file).name);
         for eventType = 1:k
 %             kernel=[];
              %for indexing rows of b easily as we loop through event types and build kernel, keep track  of timestamps (ts) that correspond to this event type 
@@ -149,9 +213,9 @@ if stage==7 && signal==465
            
            %subplot each kernel
            timeLock= linspace(-time_back, time_forward, size(kernel,1));
-           subplot(k,2,eventType);
-           plot(timeLock, kernel(:,eventType));
-           title(cons(eventType));
+%            subplot(k,2,eventType);
+%            plot(timeLock, kernel(:,eventType));
+%            title(cons(eventType));
            
            
         end
@@ -168,38 +232,23 @@ if stage==7 && signal==465
            data= data(~cellfun(@isempty,strfind(data.subject,subject)),:);
            
            %now subplot peri-event data!!
-           subplot(k,2,4); title('peri-DS');
-           hold on;
-           plot(data.timeLock, data.DSblue);
-         
-           subplot(k,2,5); title('peri-DS Pox');
-           hold on;
-           plot(data.timeLock, data.DSbluePox);
-         
-           subplot(k,2,6); title('peri-DS Lox');
-           hold on;
-           plot(data.timeLock, data.DSblueLox);
+%            subplot(k,2,4); title('peri-DS');
+%            hold on;
+%            plot(data.timeLock, data.DSblue);
+%          
+%            subplot(k,2,5); title('peri-DS Pox');
+%            hold on;
+%            plot(data.timeLock, data.DSbluePox);
+%          
+%            subplot(k,2,6); title('peri-DS Lox');
+%            hold on;
+%            plot(data.timeLock, data.DSblueLox);
          
 
            %nice plot w gramm
-           %second column with peri-event traces
+           clear i;
            figure(); hold on;
-           
-           i(1,2)=gramm('x',data.timeLock,'y',data.DSblue);
-           i(1,2).set_title('Peri-DS');
-           i(1,2).stat_summary('type','sem','geom','area');
-           i(1,2).set_names('x','time from event (s)','y','z-score');
-           
-           i(2,2)=gramm('x',data.timeLock,'y',data.DSbluePox);
-           i(2,2).set_title('Peri-DS PE');
-           i(2,2).stat_summary('type','sem','geom','area');
-           i(2,2).set_names('x','time from event (s)','y','z-score');
-           
-           i(3,2)=gramm('x',data.timeLock,'y',data.DSblueLox);
-           i(3,2).set_title('Peri-DS Lick');
-           i(3,2).stat_summary('type','sem','geom','area');
-           i(3,2).set_names('x','time from event (s)','y','z-score');
-           
+
            %first column with kernels
            %TODO: check axes
            i(1,1)=gramm('x',timeLock,'y',kernel(:,1));
@@ -217,10 +266,34 @@ if stage==7 && signal==465
            i(3,1).stat_summary('type','sem','geom','area');
            i(3,1).set_names('x','time from event (s)','y','beta');
            
-           i.set_title(mat(file).name);
-           i.draw();
            
-           linkaxes();
+           %second column with peri-event traces
+
+           i(1,2)=gramm('x',data.timeLock,'y',data.DSblue);
+           i(1,2).set_title('Peri-DS');
+           i(1,2).stat_summary('type','sem','geom','area');
+           i(1,2).set_names('x','time from event (s)','y','z-score');
+           
+           i(2,2)=gramm('x',data.timeLock,'y',data.DSbluePox);
+           i(2,2).set_title('Peri-DS PE');
+           i(2,2).stat_summary('type','sem','geom','area');
+           i(2,2).set_names('x','time from event (s)','y','z-score');
+           
+           i(3,2)=gramm('x',data.timeLock,'y',data.DSblueLox);
+           i(3,2).set_title('Peri-DS Lick');
+           i(3,2).stat_summary('type','sem','geom','area');
+           i(3,2).set_names('x','time from event (s)','y','z-score');
+           
+           
+           i.set_title(mat(file).name);
+                      
+           %manually set axes limits before drawing
+           i(:,1).axe_property('YLim',[-0.5, 1.5]);
+           i(:,2).axe_property('YLim',[-2, 5]);
+
+           i.draw();
+                    
+           linkaxes(gca,'x');
            
         saveFig(gcf, figPath, strcat(mat(file).name,'_encoding_kernels_w_periEvent'),figFormats);
 
