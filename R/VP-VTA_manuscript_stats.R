4483e81ab5345ca75e4926e0588428cb309585ab
palme876 <palme876@umn.edu>
2023-01-13 15:16:56 -0600

    cleaning up in progress; playing with assignment into df but not working

--- R/VP-VTA_manuscript_stats.R
+++ R/VP-VTA_manuscript_stats.R
@@ -4,10 +4,8 @@
 ## 2021-10-11
 ####################################################
 
-###### enter python env
-# Sys.setenv(RETICULATE_PYTHON = "C:/Users/Dakota/anaconda3/envs/r-env")
+###### enter python env ####
 Sys.setenv(RETICULATE_PYTHON = "C:/Users/Dakota/anaconda3/envs/spyder-env-seaborn-update")
-# Sys.setenv(RETICULATE_PYTHON = "C:/Users/Dakota/anaconda3/envs/r-env-from-spyder-env")
 
 #%%-- Import dependencies ####
 library(lme4)
@@ -15,9 +13,10 @@ library(reticulate)
 
 #%% -- Set Paths ####
 
-# make working directory to source file location! (up top session -> set working directory -> to source file location)
+#- NOTE: manually change working directory in RStudio to source file location! (up top session -> set working directory -> to source file location)
 # https://statisticsglobe.com/set-working-directory-to-source-file-location-automatically-in-rstudio
 
+#-Note: To read .pickles, pandas version in R environment has to match pandas version of .pkl created!
 
 pathWorking= getwd()
 
@@ -26,9 +25,6 @@ pathOutput= paste(pathWorking,'/_output', sep="")
 gsub(" ", "", pathOutput)
 
 
-
-
-#-Note: To read .pickles, pandas version in R environment has to match pandas version of .pkl created!
 
 #%% --- FIGURE 2B STATS -------------------------------------------- #### 
 
@@ -60,102 +56,93 @@ df_Sub_1= df[df$stage!="1",]
 
 #%%-- Run LME ##
 
-# formula= 'periCueBlueAuc ~ trialType * sesSpecialLabel + (1|subject)'
-# Don't use dyanmic formula name here. It won't show in the .summary()!
-# 
-# model= lmer(formula, data=df)
+library(lmerTest)
 
-model= lmer('periCueBlueAuc ~ trialType * sesSpecialLabel + (1|subject)', data=df_Sub_1)
+model= lmerTest::lmer('periCueBlueAuc ~ trialType * sesSpecialLabel + (1|subject)', data=df_Sub_1)
 
-print(model)
-
-print(summary(model))
-
-
-
-#AS example - used lmerTest too
 
 model_anova<- anova(model)
 
 
-#lmerTest may give more information when anova is called on lme (vs lmer4)
-library(lmerTest)
-
-model_lmerTest= lmerTest::lmer('periCueBlueAuc ~ trialType * sesSpecialLabel + (1|subject)', data=df_Sub_1)
-
-
-model_anova_lmerTest<- anova(model)
-
-
-#%%-- Run Follow-up post-hoc tests
-# https://stats.stackexchange.com/questions/187996/interaction-term-in-a-linear-mixed-effect-model-in-r
-# https://cran.r-project.org/web/packages/emmeans/vignettes/interactions.html
-# https://cran.r-project.org/web/packages/emmeans/vignettes/interactions.html
+#%%-- Run Follow-up post-hoc tests ####
 
 #- Signifcant interaction term, want to follow-up and estimate main effects
 
 #emmeans package useful for post-hoc
 library(emmeans)
 
-# contrast(model) #error
 
-#- Viz interaction plot
-emmip(model_lmerTest, trialType ~ sesSpecialLabel)
-
-
-#test contrast?
-# con= contrast(emms, interaction = "pairwise")
-
-
-#simple posthoc comparison: Comparison of trialType separately for each sesSpecialLabel
-t= emmeans(model_lmerTest, pairwise ~ trialType | sesSpecialLabel)
-
-summary(t)
-
-# pairwise comparisons with emmeans
-# emms<- emmeans(model_lmerTest, pairwise ~ trialType : sesSpecialLabel) #this was all interaction combos, should use |
-
-emms<- emmeans(model_lmerTest, pairwise ~ trialType | sesSpecialLabel) 
-
-#what's the difference between calling emms and calling pairs(emms) ?
-#- summary(emms) gives emmeans for each, which I think I want. Along with the pairwise contrast pvalues
-
-# PAIRS here is giving same result as emms$contrasts
-
-# https://cran.r-project.org/web/packages/emmeans/vignettes/FAQs.html#noadjust
-# emmeans() completely ignores my P-value adjustments
-# This happens when there are only two means (or only two in each by group). Thus there is only one comparison. When there is only one thing to test, there is no multiplicity issue, and hence no multiplicity adjustment to the P values.
-
-pw= pairs(emms, adjust= 'sidak')
-
-# no difference here at all
-pwDefault= pairs(emms)
-# pwNone= pairs(emms, adjust='none')
-# pwSidak= pairs(emms, adjust='sidak')
-# pwBonf= pairs(emms, adjust='bonf')
-# 
-# # assignment issue?
-# summary(pairs(emms, adjust='bonf'))
-# 
+#-- Pairwise comparisons (t test) between TrialType for each sesSpecialLabel 
 #workaround for sidak correction with only 2 groups:
-EMM <- emmeans(model_lmerTest, ~ trialType | sesSpecialLabel)   # where treat has 2 levels
-pairs(EMM, adjust = "sidak")   # adjustment is ignored - only 1 test per group
-summary(pairs(EMM), by = NULL, adjust = "sidak")   # all are in one group now
+#- Viz interaction plot & 
+figName= "fig2b_stats_1_interactionPlot.pdf"
+setwd(pathOutput)
+pdf(file=figName)
+
+emmip(model, trialType ~ sesSpecialLabel)
+
+dev.off()
+setwd(pathWorking)
 
 
-# #stage 1 
-# EMM <- emmeans(model_lmerTest, ~ trialType | sesSpecialLabel)   # where treat has 2 levels
-# pairs(EMM, adjust = "sidak")   # adjustment is ignored - only 1 test per group
-# summary(pairs(EMM), by = NULL, adjust = "sidak")   # all are in one group now
-
-EMM2 <- emmeans(model_lmerTest, ~ sesSpecialLabel | trialType)   # where treat has 2 levels
-pairs(EMM2, adjust = "sidak")   # adjustment is ignored - only 1 test per group
-summary(pairs(EMM2), by = NULL, adjust = "sidak")   # all are in one group now
+#- Pairwise T- tests
+EMM <- emmeans(model, ~ trialType | sesSpecialLabel)   # where treat has 2 levels
+tPairwise= pairs(EMM, adjust = "sidak")   # adjustment is ignored - only 1 test per group
+summary(tPairwise, by = NULL, adjust = "sidak")   # all are in one group now
 
 
-#- Viz interaction plot
-emmip(model_lmerTest, sesSpecialLabel ~ trialType)
 
+# replacing below posthoc with separate LME
+#-- Pairwise comparisons (t test) between sesSpecialLabel for each TrialType
+#workaround for sidak correction with only 2 groups:
+
+# #- Viz interaction and save plot
+# figName= "fig2b_stats_1_interactionPlot2.pdf"
+# setwd(pathOutput)
+# pdf(file=figName)
+# 
+# emmip(model,  sesSpecialLabel ~ trialType)
+# 
+# dev.off()
+# setwd(pathWorking)
+# 
+# #- Pairwise T- tests
+# EMM2 <- emmeans(model, ~ sesSpecialLabel | trialType)   # where treat has 2 levels
+# pairs(EMM2, adjust = "sidak")   # adjustment is ignored - only 1 test per group
+# summary(pairs(EMM2), by = NULL, adjust = "sidak")   # all are in one group now
+# 
+# 
+# 
+
+
+#%%-- Save output between tests  ####
+# trying to keep code mostly generalizable and just save custom names at end
+# all the results into descriptive variables between tests
+
+#naming scheme : figure_stats_{count/identifier of stats goal}_{count/identifier of stats chronology}_{descriptor of stats test}
+# trying to make names for good alphanumeric sorting / legibility later
+
+fig2B_stats_1_0_description= "DS vs NS AUC"
+fig2B_stats_1_1_model= model
+fig2B_stats_1_2_model_anova= model_anova
+fig2B_stats_1_3_model_post_hoc= tPairwise 
+
+
+#nest in df better?
+#If we want to start with a NULL object, use a list, then at the end, convert it to data.frame or data.table. In that way
+#https://stackoverflow.com/questions/64164612/error-in-data-framex-name-value-replacement-has-1-row-data-has-0
+# fig2B_stats_1= list()
+# fig2B_stats_1$model= model
+# fig2B_stats_1$model_anova= model_anova
+# 
+# fig2B_stats_1= data.frame(fig2B_stats_1)
+# 
+# 
+# fig2B_stats_1= data.frame()
+# fig2B_stats_1$model= model
+# 
+# fig2B_stats_1= data.table()
+# fig2B_stats_1$model= model
 
 
 #%% Figure 2B Stats 1 continued -- Compare DS vs Null/0 AUC on first session (no NS) --####
@@ -199,8 +186,8 @@ summary(pairs(EMM2), by = NULL, adjust = "sidak")   # all are in one group now
 
 # # Use single consistent naming structure for this fig's data?
 # #best to just keep generic & change filenames I think
-# fig2Bstats.lme= model_lmerTest
-# fig2Bstats.lme_anova= model_anova_lmerTest
+# fig2Bstats.lme= model
+# fig2Bstats.lme_anova= model_anova
 # fig2Bstats.posthoc_pairwise= emms
 
 
@@ -210,13 +197,13 @@ setwd(pathOutput)
 
 #use sink to write console output to text file
 sink("vp-vta_fig2B_lmer.txt")
-print(summary(model_lmerTest))
+print(summary(model))
 sink()  # returns output to the console
 
 
 #use sink to write console output to text file
 sink("vp-vta_fig2B_lmer_anova.txt")
-print(model_anova_lmerTest)
+print(model_anova)
 sink()  # returns output to the console
 
 
@@ -236,9 +223,9 @@ sink()  # returns output to the console
 
 sink("vp-vta_fig2B_stats.txt")
 print('1)---- LME:')
-print(summary(model_lmerTest))
+print(summary(model))
 print('2)---- ANOVA of LME:')
-print(model_anova_lmerTest)
+print(model_anova)
 print('3)---- Posthoc pairwise:')
 print(summary(t))
 
@@ -251,3 +238,22 @@ setwd(pathWorking)
 
 ## ---- FIGURE XX -----------
   
+
+
+
+## ----- Notes on Stats  / Packages-------- ####
+
+#-Don't use dynamic formula names for lmes. It won't show in the .summary()!
+
+#-lmerTest may give more information when anova is called on lme (vs lmer4)
+
+#-good resources about post-hoc testing: 
+# https://stats.stackexchange.com/questions/187996/interaction-term-in-a-linear-mixed-effect-model-in-r
+# https://cran.r-project.org/web/packages/emmeans/vignettes/interactions.html
+# https://cran.r-project.org/web/packages/emmeans/vignettes/interactions.html
+
+#- workaround if emms posthoc testing ignoring pvalue correction 'adjust':
+# https://cran.r-project.org/web/packages/emmeans/vignettes/FAQs.html#noadjust
+
+
+
